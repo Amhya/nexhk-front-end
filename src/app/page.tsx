613c7a975902d23e8bcd8884ec8e39f126e3d3e5
Amhya <ksan16esguerra@gmail.com>
2024-06-20 17:38:02 +0800

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,15 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between px-24 py-12">
       <div className="z-10 flex flex-col w-full max-w-5xl items-center justify-center  ">
-        <img className="w-[5%] aspect-square" src="/image/nextlogo.png"/>
-        <h1 className="text-3xl font-semibold text-[#2b32b2]">NEXHK</h1>
+        <img className="w-[15%] aspect-square md:w-[10%] lg:w-[5%]" src="/image/nextlogo.png"/>
+        <h1 className="text-xl md:text-3xl font-semibold text-[#2b32b2]">NEXHK</h1>
       </div>
       <div className="flex w-full max-w-5xl items-center justify-center  ">
-        <h1 className="text-7xl font-extrabold text-[#2b32b2]">COMING SOON</h1>
+        <h1 className=" font-extrabold text-center text-[#2b32b2] text-4xl md:text-6xl lg:text-7xl">COMING SOON</h1>
       </div>
       <div className="flex flex-col w-full max-w-5xl items-center justify-center ">
-        <h1 className="text-[#2b32b2]">To Pioneer the Next Way</h1>
-        <h1 className="text-[#2b32b2]">@2024 Project NEX</h1>
+        <h1 className="text-[#2b32b2] text-xs md:text-base">To Pioneer the Next Way</h1>
+        <h1 className="text-[#2b32b2] text-xs md:text-base">@2024 Project NEX</h1>
       </div>
     </main>
   );
